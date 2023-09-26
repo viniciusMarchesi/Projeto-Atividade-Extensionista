fc1cf757d975bcde93e0977aaf107a29ac563a78
Vinicius Marchesi <viniciusmarchesi2712@gmail.com>
2023-09-26 20:41:55 -0300

    atualização 1.1
    atualização 1.1

--- scripts.js
+++ scripts.js
@@ -9,6 +9,30 @@ function buscarLocais() {
         case 'vidro':
             alert('Locais de descarte para vidro: ...');
             break;
+        case 'Papelão':
+            alert('Locais de descarte para Papelão: ...');
+            break;
+        case 'Matéria orgânica':
+            alert('Locais de descarte para Matéria orgânica: ...');
+            break;
+        case 'Metais':
+            alert('Locais de descarte para Metais: ...');
+            break;
+        case 'Madeiras':
+            alert('Locais de descarte para Madeiras: ...');
+            break;
+        case 'Eletrônicos':
+            alert('Locais de descarte para Eletrônicos: ...');
+            break;  
+        case 'Eletrodomésticos':
+            alert('Locais de descarte para Eletrodomésticos: ...');
+            break;  
+        case 'Roupas':
+            alert('Locais de descarte para Roupas: ...');
+            break;
+        case 'Entulho':
+            alert('Locais de descarte para Entulho: ...');
+            break;                                
         // Adicione mais casos conforme necessário
         default:
             alert('Por favor, selecione um tipo de resíduo.');
