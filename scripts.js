09e97e5d9ebb514fb9ca270248557bfc2e6598c8
Vinicius Marchesi <viniciusmarchesi2712@gmail.com>
2023-09-30 17:44:33 -0300

    atualização 1.2
    atualização 1.2 com mapa e Lógica do Programa

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 function buscarLocais() {
     var tipoResiduo = document.getElementById('residuo').value;
+    var mapa = document.getElementById('mapa');
+    var cacamba = document.getElementById('cacamba');
 
     // Buscar os locais de descarte com base no tipo de resíduo
     switch (tipoResiduo) {
         case 'plastico':
             alert('Locais de descarte para plástico: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar o plástico na Caçamba 01 verde.";
             break;
         case 'vidro':
             alert('Locais de descarte para vidro: ...');
