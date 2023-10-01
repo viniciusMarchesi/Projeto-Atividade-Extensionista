02881a66227e6f9b695809124b4dcddeae24a69e
Vinicius Marchesi <viniciusmarchesi2712@gmail.com>
2023-10-01 20:37:10 -0300

    atualização 1.3
    atualização 1.3 com imagem logo e criação do rodapé da pagina.

--- scripts.js
+++ scripts.js
@@ -12,30 +12,48 @@ function buscarLocais() {
             break;
         case 'vidro':
             alert('Locais de descarte para vidro: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar o vidro na Caçamba 02 verde.";
             break;
         case 'Papelão':
             alert('Locais de descarte para Papelão: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar o papelão na Caçamba 03 verde.";
             break;
         case 'Matéria orgânica':
             alert('Locais de descarte para Matéria orgânica: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar a Matéria orgânica na Caçamba 03 verde.";
             break;
         case 'Metais':
             alert('Locais de descarte para Metais: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar os Metais na Casa de Concreto 02.";
             break;
         case 'Madeiras':
             alert('Locais de descarte para Madeiras: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar as Madeiras na Caçamba Branca.";
             break;
         case 'Eletrônicos':
             alert('Locais de descarte para Eletrônicos: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar os Eletrônicos na Casa de Concreto 01.";
             break;  
         case 'Eletrodomésticos':
             alert('Locais de descarte para Eletrodomésticos: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar os Eletrodomésticos na Casa de Concreto 01.";
             break;  
         case 'Roupas':
             alert('Locais de descarte para Roupas: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar as Roupas Próximo a Entrada na Caçamba de Plástico .";
             break;
         case 'Entulho':
             alert('Locais de descarte para Entulho: ...');
+            mapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.859861818305!2d-47.12787652467001!3d-22.881634636972635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c7f815bd8429%3A0xfe63d12e39ecf6ec!2sEcoponto%20Parque%20Via%20Norte!5e0!3m2!1sen!2sus!4v1696102360613!5m2!1sen!2sus";
+            cacamba.textContent = "Você deve descartar o Entulho na Caçamba 04 verde.";
             break;                                
         // Adicione mais casos conforme necessário
         default:
